cdade1be1b7acf5ca4191171b69a68824acaf2e6
Bryan Engler <bryan.engler@liferay.com>
2023-08-24 08:57:41 -0700

    LPS-194028 LPS-194526 portal-search-elasticsearch7-impl: Sort
    https://liferay.atlassian.net/browse/LPS-194526

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/index/CompanyIndexFactory.java
@@ -44,19 +44,6 @@ public class CompanyIndexFactory
 	}
 
 	@Override
-	public void initializeIndex(IndicesClient indicesClient, long companyId) {
-		String indexName = _companyIndexFactoryHelper.getIndexName(companyId);
-
-		if (_companyIndexFactoryHelper.hasIndex(indicesClient, indexName)) {
-			return false;
-		}
-
-		_companyIndexFactoryHelper.createIndex(indexName, indicesClient);
-
-		return true;
-	}
-
-	@Override
 	public void deleteIndex(IndicesClient indicesClient, long companyId) {
 		String indexName = _companyIndexFactoryHelper.getIndexName(companyId);
 
@@ -81,6 +68,17 @@ public class CompanyIndexFactory
 	@Override
 	public int getPriority() {
 		return 3;
+	}
+
+	@Override
+	public void initializeIndex(IndicesClient indicesClient, long companyId) {
+		String indexName = _companyIndexFactoryHelper.getIndexName(companyId);
+
+		if (_companyIndexFactoryHelper.hasIndex(indicesClient, indexName)) {
+			return;
+		}
+
+		_companyIndexFactoryHelper.createIndex(indexName, indicesClient);
 	}
 
 	@Override
