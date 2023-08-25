6d9f36a8a0fe64430ab49d29d745fa7dc5b510ee
Bryan Engler <bryan.engler@liferay.com>
2023-08-25 07:23:41 -0700

    LPS-194028 LPS-194526 portal-search-elasticsearch7-impl: Rename IndexFactory methods
    https://liferay.atlassian.net/browse/LPS-194526

--- modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchSearchEngine.java
+++ modules/apps/portal-search-elasticsearch7/portal-search-elasticsearch7-impl/src/main/java/com/liferay/portal/search/elasticsearch7/internal/ElasticsearchSearchEngine.java
@@ -153,13 +153,17 @@ public class ElasticsearchSearchEngine
 		boolean created = _indexFactory.createIndices(
 			restHighLevelClient.indices(), companyId);
 
-		_indexFactory.registerCompanyId(companyId);
-
-		_indexConfigurationDynamicUpdatesExecutor.execute(companyId);
-
 		if (created) {
 			_waitForYellowStatus();
 		}
+		
+		_putTimestampPipeline(restHighLevelClient);
+
+		_indexFactory.initializeIndex(restHighLevelClient.indices(), companyId);
+
+		_indexFactory.registerCompanyId(companyId);
+
+		_indexConfigurationDynamicUpdatesExecutor.execute(companyId);
 
 		CrossClusterReplicationHelper crossClusterReplicationHelper =
 			_crossClusterReplicationHelperSnapshot.get();
@@ -201,8 +205,7 @@ public class ElasticsearchSearchEngine
 			RestHighLevelClient restHighLevelClient =
 				_elasticsearchConnectionManager.getRestHighLevelClient();
 
-			_indexFactory.deleteIndices(
-				restHighLevelClient.indices(), companyId);
+			_indexFactory.deleteIndex(restHighLevelClient.indices(), companyId);
 
 			_indexFactory.unregisterCompanyId(companyId);
 		}
