50a83143ffc78bd2cee59fd75e859d0aeb8de292
Almir Ferreira <almir.ferreira@liferay.com>
2023-11-14 19:11:39 -0300

    LPS-200654 LPS-201712 portal-search-tuning-rankings-web: Change RR status to string
    https://liferay.atlassian.net/browse/LPS-201712

--- modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/RankingIndexReindexer.java
+++ modules/dxp/apps/portal-search-tuning/portal-search-tuning-rankings-web/src/main/java/com/liferay/portal/search/tuning/rankings/web/internal/index/RankingIndexReindexer.java
@@ -150,8 +150,8 @@ public class RankingIndexReindexer implements IndexReindexer {
 			JSONUtil.toStringList(jsonObject.getJSONArray("hiddenDocumentIds"))
 		).rankingDocumentId(
 			jsonObject.getString("rankingDocumentId")
-		).inactive(
-			jsonObject.getBoolean("inactive")
+		).status(
+			jsonObject.getString("status")
 		).indexName(
 			jsonObject.getString("indexName")
 		).name(
